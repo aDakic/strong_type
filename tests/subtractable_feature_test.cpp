76d0332a047347ed55b570bcdbf5d463747d1d09
Aleksandar Dakic <aleksandardakic16@gmail.com>
2021-03-27 21:19:47 +0100

    Add subtractable overload

--- tests/subtractable_feature_test.cpp
+++ tests/subtractable_feature_test.cpp
@@ -3,14 +3,17 @@
 
 TEST(test_st, subtractable_traits)
 {
-    using long_t = strong_type::strong_type<long, struct subtractable_st_tag, strong_type::subtractable,
-                                                                              strong_type::subtractable_to<long>>;
+    using int_t = strong_type::strong_type<int, struct subtractable_st_tag, strong_type::subtractable,
+                                                                            strong_type::subtractable_to<int>>;
 
-    long_t two (long_t(3) - long_t(1));
+    int_t two (int_t(3) - int_t(1));
     ASSERT_EQ(two.get(), 2);
 
-    ASSERT_EQ(long_t(3).get() - 1, 2);
+    ASSERT_EQ(int_t(3).get() - 1, 2);
 
-    long_t one(two - 1);
+    int_t one(two - 1);
     ASSERT_EQ(one.get(), 1);
+
+    int_t four = 5 - int_t(1);
+    ASSERT_EQ(four.get(), 4);
 }
